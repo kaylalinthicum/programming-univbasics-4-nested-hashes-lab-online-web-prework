be5e973122250b3304fcd3dbdac73949cbd0c364
Kayla Linthicum <kaylalinthicum@gmail.com>
2019-11-24 23:07:38 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -82,7 +82,7 @@ def adding_matz
         }
      }
 
-  programmer_hash.unshift[:yukihiro_matsumoto] = :known_for = "Ruby", :languages = "LISP", "C"
+  programmer_hash.unshift[:yukihiro_matsumoto]
 end
 
 def changing_alan
