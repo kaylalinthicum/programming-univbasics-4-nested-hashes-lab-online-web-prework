194a647cbd7851f1e962a2639ae5871a765acd92
Kayla Linthicum <kaylalinthicum@gmail.com>
2019-11-24 23:45:49 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -124,6 +124,7 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-  dennis_new_languages = ["C", "Assembly"]
-  programmer_hash[:dennis_ritchie][:languages]
+    dennis_new_languages = ["C", "Assembly"]
+    programmer_hash[:dennis_ritchie][:languages] = dennis_new_languages
+  programmer_hash
 end
